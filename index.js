5e3cf5a9903c7e8a1525fa0c264e9c1ab04a7132
pichonrobin <robin.pichon@ynov.com>
2018-05-14 10:27:26 +0200

    exo1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-Commande ECHO activ‚e.
+const fs = require('fs');
+setInterval (write, 1000)
+
+function write () {
+  const actual = Date.now()
+  console.log( actual);
+  fs.appendFileSync('message.txt', actual + "\n");
+}
